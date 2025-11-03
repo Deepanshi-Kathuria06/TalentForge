d300c174ada6320f946a44b5111dc6dcfadeba8d
Deepanshi <kathuriadeepanshi56@gmail.com>
2025-11-03 14:40:46 +0530

    Configured API URL for production

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,11 @@
 // utils/api.js
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000/api" });
+
+const API = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+});
+
 // ✅ Add token to every request
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
