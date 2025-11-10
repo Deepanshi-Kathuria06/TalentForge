9dc92711ee199a48166e0d7bb03cdc32fee8ee14
Deepanshi <kathuriadeepanshi56@gmail.com>
2025-11-10 18:03:00 +0530

    finished

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,11 +3,18 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "./",
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      external: []
+    }
+  },
   server: {
     port: 5178,
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://talentforge-w4t2.onrender.com",
         changeOrigin: true,
         secure: false
       }
